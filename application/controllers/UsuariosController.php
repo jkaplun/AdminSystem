40712ea6c261818d6ba07819cb38e480984e2867
Francisco Arellano <pco0309@gmail.com>
2016-09-19 23:19:47 -0500

    Vista Usuarios
    Es la vista de los usuarios

--- application/controllers/UsuariosController.php
+++ application/controllers/UsuariosController.php
@@ -11,73 +11,79 @@ class UsuariosController extends Zend_Controller_Action
 
     public function indexAction()
     {
-     //    // action body
+        //action body
 
 
         $mensaje = 'mensaje de prueba';
         $this->view->mensaje=$mensaje;
 
 
-    	// $this->view->InlineScript()->appendFile($this->view->baseUrl().'/js/admin/index.js');
-    	// $users = new Application_Model_DbTable_Users();
-    	// $params=$this->_request->getParams();
+    	 $this->view->InlineScript()->appendFile($this->view->baseUrl().'/js/admin/index.js');
+    	 $users = new Application_Model_DbTable_UsuarioAdmin();
+    	 $params=$this->_request->getParams();
 
-    	// if( $this->_request->isPost() ){
-    	// 	//echo '<pre>'.print_r($params,true).'</pre>';die;
-    	// 	$users = new Application_Model_DbTable_Users();
+    	 if( $this->_request->isPost() ){
+    	 	echo '<pre>'.print_r($params,true).'</pre>';die;
+    	 	$users = new Application_Model_DbTable_UsuarioAdmin();
     		
-    	// 	if ( $params['accion'] == 'editar' ){
-	    // 		$data = array(
-	    // 				'realname' => trim($params['edita_nombre_real']),
-	    // 				'activo' => $params['edita_activo'],
-	    // 				'id_rol' => $params['edita_rol']
-	    // 		);
+    	 	if ( $params['accion'] == 'editar' ){
+	     		$data = array(
+	     				'nombre' => trim($params['edita_nombre']),
+                        'apellido_paterno' => trim($params['edita_apellido_paterno']),
+                        'apellido_materno' => trim($params['edita_apellido_materno']),
+	     				'activo' => $params['edita_activo'],
+                        'puesto' => $params['edita_puesto'],
+                        'email' => $params['edita_email']
+	     		);
 	    		
-	    // 		if ($params['edita_password'] != '' && $params['edita_password']==$params['edita_passwordConfirm']){
-	    // 			$data['password'] = sha1(trim($params['edita_password']));
-	    // 		}
+	     		if ($params['edita_password'] != '' && $params['edita_password']==$params['edita_passwordConfirm']){
+	     			$data['password'] = sha1(trim($params['edita_password']));
+	     		}
 	
-	    // 		$where = "id_user = ".$params['edita_id_user'];
+	     		$where = "id_usuario = ".$params['edita_id_user'];
 	    		
-	    // 		$users->update($data, $where);
+	     		$users->update($data, $where);
     		
-    	// 	}
+    	 	}
     		
-    	// 	if ( $params['accion'] == 'agregar' ){
+    	 	if ( $params['accion'] == 'agregar' ){
     			
-    	// 		$data = array(
-    	// 				'username' => trim($params['username']),
-    	// 				'realname' => trim($params['nombre_real']),
-    	// 				'activo' => 1,
-    	// 				'id_rol' => $params['rol'],
-    	// 				'password' => sha1(trim($params['password']))
-    	// 		);
-    	// 		//echo '<pre>'.print_r($data,true).'</pre>';die;
-    	// 		$users->insert($data);
-    	// 	}
+    	 		$data = array(
+    	 				'clave' => trim($params['clave']),
+    	 				'realname' => trim($params['nombre_real']),
+    	 				'activo' => 1,
+    	 				'id_rol' => $params['rol'],
+    	 				'password' => sha1(trim($params['password']))
+    	 		);
+    	 		echo '<pre>'.print_r($data,true).'</pre>';die;
+    	 		$users->insert($data);
+    	 	}
     		
-    	// }
+    	 }
     	
-    	// unset($params['controller']);
-    	// unset($params['action']);
-    	// unset($params['module']);
-    	// $registros = $users->getAllUsers($params);
+    	 unset($params['controller']);
+    	 unset($params['action']);
+    	 unset($params['module']);
+    	 $registros = $users->getAllUsers($params);
     	 
-    	// //$this->view->registros = $registros;
-    	// $this->view->countArray= count($registros);
+    	 $this->view->registros = $registros;
+    	 $this->view->countArray= count($registros);
     	
-    	// // Get a Paginator object using Zend_Paginator's built-in factory.
-    	// $page = $this->_request->getParam('page', 0);
-    	// //$page = 0;
-    	// $paginator = Zend_Paginator::factory($registros);
-    	// $paginator->setCurrentPageNumber($page)
-    	// ->setItemCountPerPage(10)
-    	// ->setPageRange(10);
-    	// //Zend_Paginator::setCache($cache);
-    	// $paginator->setCacheEnabled(true);
-    	// // Assign paginator to view
-    	// Zend_View_Helper_PaginationControl::setDefaultViewPartial('pagination_sm.phtml');
-    	// $this->view->paginator=$paginator;
+        //$this->view->registros = $registros;
+        $this->view->countArray= count($registros);
+        
+        // Get a Paginator object using Zend_Paginator's built-in factory.
+        $page = $this->_request->getParam('page', 0);
+        //$page = 0;
+        $paginator = Zend_Paginator::factory($registros);
+        $paginator->setCurrentPageNumber($page)
+        ->setItemCountPerPage(10)
+        ->setPageRange(10);
+        //Zend_Paginator::setCache($cache);
+        $paginator->setCacheEnabled(true);
+        // Assign paginator to view
+        Zend_View_Helper_PaginationControl::setDefaultViewPartial('pagination_sm.phtml');
+        $this->view->paginator=$paginator;
     }
 
 
