08a422431ab5cce60c665115f5296ed86b69aa59
juan <ing.juan.garfias@gmail.com>
2016-09-21 07:02:06 -0500

    Restablecí las modificaciones de paquito.

--- application/controllers/UsuariosController.php
+++ application/controllers/UsuariosController.php
@@ -12,34 +12,12 @@ class UsuariosController extends Zend_Controller_Action
     public function indexAction()
     {
         //action body
+
+
         $mensaje = 'mensaje de prueba';
         $this->view->mensaje=$mensaje;
-    }
-    
-    public function agregarAction(){
-    	$params=$this->_request->getParams();
 
-        $data = array(
-            'clave' => $params['clave'],
-            'pwd' => $params['pwd'],
-            'nombre' => $params['nombre'],
-            'apellido_paterno' => $params['apellido_paterno'],
-            'apellido_materno' => $params['apellido_materno'],
-            'puesto' => $params['puesto'],
-            'email' => $params['email'],
-            'admin' => $params['admin'],
-            'supervisor' => $params['supervisor'],
-            'folios' => $params['folios'],
-            'compila' => $params['compila'],
-            'activo' => $params['activo']
-            );
-        $this->usuario_admin->insert($data);
 
-    	$this->_redirect('usuarios/');
-    }
-    
-    public function actualizarAction(){
-        $params=$this->_request->getParams();
     	 $this->view->InlineScript()->appendFile($this->view->baseUrl().'/js/admin/index.js');
     	 $users = new Application_Model_DbTable_UsuarioAdmin();
     	 $params=$this->_request->getParams();
