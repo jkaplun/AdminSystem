6e593c083d8c81923322335da81f29a4bd70431d
Gaby <gabymg91@hotmail.com>
2016-09-22 02:32:57 -0600

    Subiendo de nuevo cambios. Hubo un error en el último merge.

--- application/models/DbTable/UsuarioAdmin.php
+++ application/models/DbTable/UsuarioAdmin.php
@@ -3,42 +3,49 @@
 class Application_Model_DbTable_UsuarioAdmin extends Zend_Db_Table_Abstract
 {
 
-	protected $_name = 'usuario_admin';
+  protected $_name = 'usuario_admin';
 
-	public function getUserValues ($values)
-	{
-	$select = $this->_db->select()->
-		from ( $this->_name, '*' )
-		->where('clave="'.$values['clave'].'" and pwd=sha("'.$values['pwd'].'")');
+  public function getUserValues ($values)
+  {
+  $select = $this->_db->select()->
+    from ( $this->_name, '*' )
+    ->where('clave="'.$values['clave'].'" and pwd=sha("'.$values['pwd'].'")');
 
-		return $this->getAdapter ()->fetchRow ( $select );
-	}
-	
-	public function getAllUsers ($values)
-	{
-		
-		$cond = 'clave is not null ';
-		
-		if( trim(@$values['usuario_admin']) !=''){
-			$cond .= ' and clave like "%'.$values['usuario_admin'].'%"';
-		}
-		
-		$select = $this->_db->select()->
-		from ( $this->_name, 
-				array(
-						'id_usuario',
-						'clave',
-						'nombre',
-						'apellido_paterno',
-						'apellido_materno',
-						'activo',
-						'email'))
-				->where($cond);;
-	
-		//echo $select;die;
-		//$result = $this->getAdapter ()->fetchRow ( $select );
-	
-		return $this->getAdapter ()->fetchAll ( $select );
-	}
+    return $this->getAdapter ()->fetchRow ( $select );
+  }
+  
+  public function getAllUsers ($values)
+  {
+    
+    $cond = 'clave is not null ';
+    
+    if( trim(@$values['usuario_admin']) !=''){
+      $cond .= ' and clave like "%'.$values['usuario_admin'].'%"';
+    }
+    
+    $select = $this->_db->select()->
+    from ( $this->_name, 
+        array(
+            'id_usuario',
+            'clave',
+            'nombre',
+            'apellido_paterno',
+            'apellido_materno',
+            'activo',
+            'email'))
+        ->where($cond);;
+  
+    return $this->getAdapter ()->fetchAll ( $select );
+  }
+  
+  public function obtenerUsuarioPorClave ($clave)
+  {
+    $select = $this->_db->select()->
+      from ( $this->_name, '*' )
+      ->where('clave="'.$clave.'"');
+
+      //echo $select;die;
+    return $this->getAdapter ()->fetchAll ( $select );
+  }
 }
 
