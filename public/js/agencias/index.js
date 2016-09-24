759de6380c803d0f5936685b2c9acfe8080ab9c9
evaldez <ericvv90@gmail.com>
2016-09-24 14:31:56 -0500

    Ya se agrega agencia a la  BD mediante la petición AJAX
    Fallaba siempre la verificación del RFC (siempre mandaba falso) por lo que se comentó esa linea (siempre es true);

--- public/js/agencias/index.js
+++ public/js/agencias/index.js
@@ -1,3 +1,9 @@
+
+var ajaxAction = "agregar";
+var path="public/agencias/";
+
+
+
 $(document).ready(function() {
     $('#dataTable-agencias').DataTable({
         responsive: true,
@@ -27,8 +33,36 @@ $(document).ready(function() {
 		}
 
     });
-});
+
+
+//$("#submitBtn").click(submit());
+
+
+}); // end  $(document).ready(function() {
 
 function abrirModalAgregarAgencia(){
 	$('#myModalLabel').html("Agregar Agencia");
+}
+
+
+function submitForm(){
+
+	console.log("parametros a mandar: ");
+	console.log($("#form_agregar_agencia").serialize());
+	$.ajax({
+		  url: path + ajaxAction,
+		  method: "post",
+		  data: $("#form_agregar_agencia").serialize() ,
+		  dataType: "json"
+		})
+		.done(function(res) { 
+
+			swal("respuesta del cliente: "+res.estado+ " descripcion: "+res.descripcion);
+
+  })// end ajax done 
+		.fail(function() {
+    	swal("Error :(", "ocurrió un error con el servidor, por favor intentelo más tarde ", "error" );
+  });
+	
+
 }
