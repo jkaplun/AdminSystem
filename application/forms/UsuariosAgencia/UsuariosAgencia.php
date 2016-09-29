535c90a9f89d98936a9331e3af3326275d0b9ea0
Gaby <gabymg91@hotmail.com>
2016-09-28 23:51:34 -0600

    Se ordenaron alfabéticamente las agencias.

---
+++ application/forms/UsuariosAgencia/UsuariosAgencia.php
@@ -0,0 +1,268 @@
+<?php
+// located at application/forms/UsuariosAgencia/UsuariosAgencia.php
+ 
+class Application_Form_UsuariosAgencia_UsuariosAgencia extends Zend_Form
+{
+    public function init()
+    {
+        // Clave
+        $clave = new Zend_Form_Element_Text('clave');
+        $clave->setRequired(true)
+        ->addErrorMessage("- Es necesario que introduzca el nombre de usuario.")
+        ->removeDecorator('label')
+        ->removeDecorator('HtmlTag')
+        ->setAttrib("class","form-control")
+        ->setAttrib("autocomplete","off")
+        ->setAttrib("placeholder",utf8_encode("Usuario"))
+        ->setAttrib("maxlength","45")
+        //->setAttrib('disabled', 'disabled');
+        ;
+        
+        // Password
+        $password = new Zend_Form_Element_Password('pwd');
+        $password->setRequired(true)
+        ->addErrorMessage("- Es necesario que introduzca el password.")
+        ->removeDecorator('label')
+        ->removeDecorator('HtmlTag')
+        ->setAttrib("autocomplete","off")
+        ->setAttrib("class","form-control")
+        ->setAttrib("placeholder","Password")
+        ->setAttrib("maxlength","45")
+        ;
+        
+        
+        // Password
+        $password_conf = new Zend_Form_Element_Password('pwd_conf');
+        $password_conf
+        ->setRequired(true)
+        ->addErrorMessage("- Es necesario que introduzca el password.")
+        ->removeDecorator('label')
+        ->removeDecorator('HtmlTag')
+        ->setAttrib("autocomplete","off")
+        ->setAttrib("class","form-control")
+        ->setAttrib("placeholder","Confirmar password")
+        ->setAttrib("maxlength","45")
+        ;
+        
+        // Nombre
+        $nombre = new Zend_Form_Element_Text('nombre');
+        $nombre->setRequired(true)
+        ->addErrorMessage("- Es necesario que introduzca el nombre.")
+        ->removeDecorator('label')
+        ->removeDecorator('HtmlTag')
+        ->setAttrib("class","form-control")
+        ->setAttrib("autocomplete","off")
+        ->setAttrib("placeholder",utf8_encode("Nombre"))
+        ->setAttrib("maxlength","75")
+        ;
+        
+        // Apellido paterno
+        $apellido_paterno = new Zend_Form_Element_Text('apellido_paterno');
+        $apellido_paterno->setRequired(true)
+        ->addErrorMessage("- Es necesario que introduzca el apellido paterno.")
+        ->removeDecorator('label')
+        ->removeDecorator('HtmlTag')
+        ->setAttrib("class","form-control")
+        ->setAttrib("autocomplete","off")
+        ->setAttrib("placeholder",utf8_encode("Apellido paterno"))
+        ->setAttrib("maxlength","75")
+        ;
+        
+        // Apellido materno
+        $apellido_materno = new Zend_Form_Element_Text('apellido_materno');
+        $apellido_materno->setRequired(true)
+        ->addErrorMessage("- Es necesario que introduzca el apellido materno.")
+        ->removeDecorator('label')
+        ->removeDecorator('HtmlTag')
+        ->setAttrib("class","form-control")
+        ->setAttrib("autocomplete","off")
+        ->setAttrib("placeholder",utf8_encode("Apellido materno"))
+        ->setAttrib("maxlength","75")
+        ;
+        
+        // Puesto
+        $puesto = new Zend_Form_Element_Text('puesto');
+        $puesto->setRequired(true)
+        ->addErrorMessage("- Es necesario que introduzca el puesto.")
+        ->removeDecorator('label')
+        ->removeDecorator('HtmlTag')
+        ->setAttrib("class","form-control")
+        ->setAttrib("autocomplete","off")
+        ->setAttrib("placeholder",utf8_encode("Puesto"))
+        ->setAttrib("maxlength","75")
+        ;
+        
+        // Email
+        $email = new Zend_Form_Element_Text('email');
+        $email->setRequired(true)
+        ->addErrorMessage("- Es necesario que introduzca el email")
+        ->removeDecorator('label')
+        ->removeDecorator('HtmlTag')
+        ->setAttrib("class","form-control")
+        ->setAttrib("autocomplete","off")
+        ->setAttrib("placeholder",utf8_encode("Email"))
+        ->setAttrib("maxlength","100")
+        ;
+        
+        // Activo
+        $activo = new Zend_Form_Element_Select('activo');
+        $activo->setAttribs ( array (
+                'autocomplete'=>'off'))
+                ->addMultiOptions(array(
+                        'S'=>'Activo',
+                        'N'=>'Inactivo'
+                ))
+                ->setAttrib("class","form-control")                
+                ->removeDecorator('label')
+                ->setValue('si')
+                ->removeDecorator('HtmlTag');
+        ;
+        $this->addElement($activo);
+        
+        // Líder de proyecto
+        $lider_proy = new Zend_Form_Element_Select('lider_proy');
+        $lider_proy->setRequired(true)
+        ->removeDecorator('label')
+        ->removeDecorator('HtmlTag')
+        ->addMultiOptions(array(
+                'S'=>'Activo',
+                'N'=>'Inactivo'
+        ))
+        ->setAttrib("class","form-control")
+        ->setAttrib("autocomplete","off")
+        ;
+        $this->addElement($lider_proy);
+        
+        // Director
+        $director = new Zend_Form_Element_Select('director');
+        $director->setRequired(true)
+        ->removeDecorator('label')
+        ->removeDecorator('HtmlTag')
+        ->addMultiOptions(array(
+                'S'=>'Activo',
+                'N'=>'Inactivo'
+        ))
+        ->setAttrib("class","form-control")
+        ->setAttrib("autocomplete","off")
+        ;
+        $this->addElement($director);
+        
+        // admin_FE
+        $admin_fe = new Zend_Form_Element_Select('admin_fe');
+        $admin_fe->setRequired(true)
+        ->removeDecorator('label')
+        ->removeDecorator('HtmlTag')
+        ->addMultiOptions(array(
+                'S'=>'Activo',
+                'N'=>'Inactivo'
+        ))
+        ->setAttrib("class","form-control")
+        ->setAttrib("autocomplete","off")
+        ;
+        $this->addElement($admin_fe);
+        
+        // nuevo_user
+        $nuevo_user = new Zend_Form_Element_Select('nuevo_user');
+        $nuevo_user->setRequired(true)
+        ->removeDecorator('label')
+        ->removeDecorator('HtmlTag')
+        ->addMultiOptions(array(
+                'S'=>'Activo',
+                'N'=>'Inactivo'
+        ))
+        ->setAttrib("class","form-control")
+        ->setAttrib("autocomplete","off")
+        ;
+        $this->addElement($nuevo_user);
+        
+        // actualizar_pass
+        $actualizar_pass = new Zend_Form_Element_Select('actualizar_pass');
+        $actualizar_pass->setRequired(true)
+        ->removeDecorator('label')
+        ->removeDecorator('HtmlTag')
+        ->addMultiOptions(array(
+                'S'=>'Activo',
+                'N'=>'Inactivo'
+        ))
+        ->setAttrib("class","form-control")
+        ->setAttrib("autocomplete","off")
+        ;
+        $this->addElement($actualizar_pass);
+        
+        // Enviar reporte portal MIG
+        $enviar_reporte_portal_mig = new Zend_Form_Element_Select('enviar_reporte_portal_mig');
+        $enviar_reporte_portal_mig->setRequired(true)
+        ->removeDecorator('label')
+        ->removeDecorator('HtmlTag')
+        ->addMultiOptions(array(
+                'S'=>'Activo',
+                'N'=>'Inactivo'
+        ))
+        ->setAttrib("class","form-control")
+        ->setAttrib("autocomplete","off")
+        ;
+        $this->addElement($enviar_reporte_portal_mig);
+        
+        // Bajar updates
+        $bajar_updates = new Zend_Form_Element_Select('bajar_updates');
+        $bajar_updates->setRequired(true)
+        ->removeDecorator('label')
+        ->removeDecorator('HtmlTag')
+        ->addMultiOptions(array(
+                'S'=>'Activo',
+                'N'=>'Inactivo'
+        ))
+        ->setAttrib("class","form-control")
+        ->setAttrib("autocomplete","off")
+        ;
+        $this->addElement($bajar_updates);
+        
+        // Teléfono
+        $telefono = new Zend_Form_Element_Text('telefono');
+        $telefono->setRequired(true)
+        ->addErrorMessage("- Es necesario que introduzca el teléfono")
+        ->removeDecorator('label')
+        ->removeDecorator('HtmlTag')
+        ->setAttrib("class","form-control")
+        ->setAttrib("autocomplete","off")
+        ->setAttrib("placeholder",utf8_encode("Email"))
+        ->setAttrib("maxlength","50")
+        ;
+        $this->addElement($telefono);
+        
+        // Extensión
+        $extension = new Zend_Form_Element_Text('extension');
+        $extension ->removeDecorator('label')
+        ->removeDecorator('HtmlTag')
+        ->setAttrib("class","form-control")
+        ->setAttrib("autocomplete","off")
+        ->setAttrib("placeholder",utf8_encode("ext"))
+        ->setAttrib("maxlength","10")
+        ;
+        $this->addElement($extension);
+        
+        // Celular
+        $celular = new Zend_Form_Element_Text('celular');
+        $celular->removeDecorator('label')
+        ->removeDecorator('HtmlTag')
+        ->setAttrib("class","form-control")
+        ->setAttrib("autocomplete","off")
+        ->setAttrib("placeholder",utf8_encode("Celular"))
+        ->setAttrib("maxlength","20")
+        ;
+        $this->addElement($celular);
+        
+        // Submit
+        $submit = new Zend_Form_Element_Submit('submit');
+        $submit ->setLabel("Iniciar")
+        ->setAttrib("class","btn btn-lg btn-success btn-block")
+        ->removeDecorator('label')
+        ->removeDecorator('HtmlTag')
+        ->removeDecorator('Errors')
+        ;
+        $id_usuario = new Zend_Form_Element_Hidden('id_usuario');
+        
+        $this
+        ->setMethod('post');
+      }
+}
