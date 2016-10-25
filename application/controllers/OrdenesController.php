a27550cd5d1b8a402146861bac998d009e5e60a1
Francisco Arellano <pco0309@gmail.com>
2016-10-25 09:33:09 -0500

    Ordenes

--- application/controllers/OrdenesController.php
+++ application/controllers/OrdenesController.php
@@ -19,6 +19,15 @@ class OrdenesController extends Zend_Controller_Action
 
     }
 
+    public function seguimientoOrdenAction()
+    {      
+        $this->view->InlineScript()->appendFile($this->view->baseUrl().'/css_complete/datatables/js/jquery.dataTables.min.js');
+        $this->view->InlineScript()->appendFile($this->view->baseUrl().'/css_complete/datatables-plugins/dataTables.bootstrap.min.js');
+        $this->view->InlineScript()->appendFile($this->view->baseUrl().'/css_complete/datatables-responsive/dataTables.responsive.js'); 
+        $this->view->InlineScript()->appendFile($this->view->baseUrl().'/js/ordenes/seguimiento-ordenes.js');
+
+    }
+
     public function monitoreoOrdenAction()
     {    
         $this->_helper->layout->setLayout('layout_monitoreo');  
---
+++ public/js/ordenes/seguimiento-ordenes.js
@@ -0,0 +1,25 @@
+$(document).ready(function() { 
+  $('#dataTable-seguimientoOrdenes').DataTable({
+        responsive: true,
+        "language":{
+          "sProcessing":     "Procesando...",
+          "sLengthMenu":     "Mostrar _MENU_ registros",
+          "sZeroRecords":    "No se encontraron resultados",
+          "sEmptyTable":     "Ning&uacute;n dato disponible en esta tabla",
+          "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+          "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+          "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+          "sInfoPostFix":    "",
+          "sSearch":         "Buscar:",
+          "sUrl":            "",
+          "sInfoThousands":  ",",
+          "sLoadingRecords": "Cargando...",
+          "oPaginate": {
+              "sFirst":    "Primero",
+              "sLast":     "&Uacute;timo",
+              "sNext":     "Siguiente",
+              "sPrevious": "Anterior"
+          }
+        }
+    });
+}); // end  $(document).ready(function() { 
