03dbdde7e23dd295fe38c8003a59589c072e09af
juan <ing.juan.garfias@gmail.com>
2016-10-31 17:56:28 -0600

    Comentarios tres.

--- application/models/DbTable/OrdenServicio.php
+++ application/models/DbTable/OrdenServicio.php
@@ -2,7 +2,8 @@
 /**
  * 
  * @author Juan Garfias
- * Clase de Orden de Servicio
+ * Clase de Orden de Servicio.
+ * 
  *
  */
 class Application_Model_DbTable_OrdenServicio extends Zend_Db_Table_Abstract
