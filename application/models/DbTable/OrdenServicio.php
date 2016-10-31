e8f99ce66c117ddb0bf7d8c74f0474b04ee8d367
juan <ing.juan.garfias@gmail.com>
2016-10-30 19:36:05 -0600

    Modificación de nombre de vista.

--- application/models/DbTable/OrdenServicio.php
+++ application/models/DbTable/OrdenServicio.php
@@ -9,7 +9,7 @@ class Application_Model_DbTable_OrdenServicio extends Zend_Db_Table_Abstract
 	public function obtenerOrdenes($valores){
 		
 		$select = $this->_db->select()->
-		from ( "view_orde_servicio", '*' );
+		from ( "view_orden_servicio", '*' );
 		
 		//echo $select;
 		//die;
