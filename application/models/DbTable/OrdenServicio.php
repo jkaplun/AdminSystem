7d89d646ebb479498addb7f3a3a67ca660056bcf
Francisco Arellano <pco0309@gmail.com>
2016-11-02 15:44:56 -0600

    Update Descripcion Monitoreo

--- application/models/DbTable/OrdenServicio.php
+++ application/models/DbTable/OrdenServicio.php
@@ -24,7 +24,7 @@ class Application_Model_DbTable_OrdenServicio extends Zend_Db_Table_Abstract
 	public function obtenerOrdenesMonitoreo()
 	{
 		$select = $this->_db->select()->
-		from ( $this->_name,
+		from (array('p' => $this->_name),
 				array(
 					'id_orden_servicio',
 					'id_agencia',
@@ -34,6 +34,15 @@ class Application_Model_DbTable_OrdenServicio extends Zend_Db_Table_Abstract
 					'id_orden_servicio_estatus'
 					)
 		)
+		->joinleft(array('u_a' => 'usuario_agencia'),
+						'p.id_usuario_agencia_solicito = u_a.id_usuario_agencia',
+						array('nombre as nombre_usuario', 'apellidos as apellidos_usuario', 'puesto'))
+		->joinleft(array('u_ad' => 'usuario_admin'),
+						'p.id_usuario_admin_atiende = u_ad.id_usuario',
+						array('nombre as nombre_atiende', 'apellido_paterno'))
+		->joinleft(array('a' => 'agencia'),
+						'p.id_agencia = a.id_agencia',
+						array('nombre as nombre_agencia'))		
 		->where("concluido = 'N'");
 		return $this->getAdapter ()->fetchAll( $select );
 	}
