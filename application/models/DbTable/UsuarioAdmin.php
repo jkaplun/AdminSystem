0709d607c6bc1ab21a8ed1431b6f15e4683919fe
juan Pc/Miga <ing.juan.garfias@gmail.com>
2017-05-05 16:04:58 -0500

    Correción Monitor.

--- application/models/DbTable/UsuarioAdmin.php
+++ application/models/DbTable/UsuarioAdmin.php
@@ -89,7 +89,7 @@ class Application_Model_DbTable_UsuarioAdmin extends Zend_Db_Table_Abstract
   }
 
   public function getSoporteUsers(){
-  	$cond = 'es_ejecutivo="S"';
+  	$cond = 'es_ejecutivo="S" AND activo="S"';
   	$select = $this->_db->select()
   	->from( $this->_name,'*')
   	->where($cond);
