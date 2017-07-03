1d47fca417ac93dd01942bcb22aa471145a583fa
juan Pc/Miga <ing.juan.garfias@gmail.com>
2017-07-03 16:32:11 -0500

    Correción para el monitor.

--- application/models/DbTable/UsuarioAdmin.php
+++ application/models/DbTable/UsuarioAdmin.php
@@ -89,7 +89,7 @@ class Application_Model_DbTable_UsuarioAdmin extends Zend_Db_Table_Abstract
   }
 
   public function getSoporteUsers(){
-  	$cond = 'es_ejecutivo="S" AND activo="S"';
+  	$cond = 'activo="S" AND en_monitor="S"';
   	$select = $this->_db->select()
   	->from( $this->_name,'*')
   	->where($cond)
